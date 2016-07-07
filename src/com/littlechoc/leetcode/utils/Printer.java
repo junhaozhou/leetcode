ebbfafb957a06cd158c561871a585964a61bd1d0
littlechoc <junhao.zhou@foxmail.com>
2016-07-07 17:42:02 +0800

    添加工具类包，打印工具类Printer

---
+++ src/com/littlechoc/leetcode/utils/Printer.java
@@ -0,0 +1,15 @@
+package com.littlechoc.leetcode.utils;
+
+/**
+ * Created by Lttlechoc on 2016/7/7 0007.
+ */
+public class Printer {
+
+  public static void print(String s) {
+    System.out.print(s);
+  }
+
+  public static void printTime(long second) {
+    System.out.print("--------- Runtime: " + second + " ms ---------");
+  }
+}
