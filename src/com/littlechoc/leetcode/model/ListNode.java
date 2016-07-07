35d85665d68946c13f5e6160f1175f19e7b9330f
littlechoc <junhao.zhou@foxmail.com>
2016-07-07 17:40:32 +0800

    add data structure ListNode created by LeetCode.

---
+++ src/com/littlechoc/leetcode/model/ListNode.java
@@ -0,0 +1,14 @@
+package com.littlechoc.leetcode.model;
+
+/**
+ * Definition for singly-linked list.
+ * By LeetCode.
+ */
+public class ListNode {
+  public int val;
+  public ListNode next;
+
+  ListNode(int x) {
+    val = x;
+  }
+}
