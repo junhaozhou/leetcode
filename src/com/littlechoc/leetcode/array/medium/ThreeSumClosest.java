d3d7ac57e5b90118498f7c03fc1f53992fea79ab
littlechoc <junhao.zhou@foxmail.com>
2016-07-14 13:28:38 +0800

    feat: 添加新问题
    -添加143号问题：ReorderList
    -添加16号问题：ThreeSumClosest

---
+++ src/com/littlechoc/leetcode/array/medium/ThreeSumClosest.java
@@ -0,0 +1,31 @@
+package com.littlechoc.leetcode.array.medium;
+
+/**
+ * Problem 16. See in <a href="https://leetcode.com/problems/3sum-closest/">LeetCode</a>.
+ *
+ * @author 周俊皓.
+ *         2016-07-14 13:14
+ */
+public class ThreeSumClosest {
+
+  public int threeSumClosest(int[] nums, int target) {
+    int length = nums.length;
+    int min = Integer.MAX_VALUE;
+    int sum = 0;
+    for (int i = 0; i < length; ++i) {
+      for (int j = i + 1; j < length; ++j) {
+        for (int k = j + 1; k < length; ++k) {
+          int sumTemp = nums[i] + nums[j] + nums[k];
+          int diff = Math.abs(target - sumTemp);
+          min = min < diff ? min : diff;
+          sum = min < diff ? sum : sumTemp;
+          if (min == 0) {
+            return sumTemp;
+          }
+        }
+      }
+    }
+    return sum;
+  }
+
+}
---
+++ src/com/littlechoc/leetcode/linkedlist/medium/ReorderList.java
@@ -0,0 +1,61 @@
+package com.littlechoc.leetcode.linkedlist.medium;
+
+import com.littlechoc.leetcode.model.ListNode;
+
+/**
+ * Problem 143. See in <a herf="https://leetcode.com/problems/reorder-list/">LeetCode</a>
+ *
+ * @author 周俊皓.
+ *         2016-07-14 11:33
+ */
+public class ReorderList {
+
+  private ListNode start;
+
+  public void reorderList(ListNode head) {
+
+    // 1. find the middle point
+    if (head == null || head.next == null || head.next.next == null) return;
+
+    ListNode middle = head, end = head;
+
+    while (end.next != null) {
+      // a1 step = 1
+      middle = middle.next;
+      // a2 step = 2
+      end = end.next;
+      if (end.next == null) break;
+      else end = end.next;
+    }
+    // middle now points to middle, end points to last node
+
+    // 2. reverse the second half of the list
+    this.reverseList(middle);
+
+    // 3. merge two lists
+    ListNode p = head, t1 = head, t2 = head;
+    while (end != middle) { // start from both side of the list. when a1, a2 meet, the merge finishes.
+      t1 = p;
+      t2 = end;
+      p = p.next;
+      end = end.next;
+
+      t2.next = t1.next;
+      t1.next = t2;
+    }
+  }
+
+  // use recursion to reverse the right part of the list
+  private ListNode reverseList(ListNode head) {
+    ListNode reverse = null;
+    ListNode next;
+    while (head != null) {
+      next = head.next;
+      head.next = reverse;
+      reverse = head;
+      head = next;
+    }
+    return reverse;
+  }
+
+}
